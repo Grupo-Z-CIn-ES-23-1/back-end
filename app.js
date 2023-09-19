dbcb49a923236e1c8e7502526d7312fdb342ef9b
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-09-19 01:55:24 -0300

    feat: added swagger to manage documentation

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API for FloodForecasting APP",
+    version: "1.0.0",
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // DB Connection
 const conn = require("./db/conn");
